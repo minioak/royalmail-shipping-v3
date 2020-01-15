d80eaf07324b56bc6002e5c711d8bfffa525011b
Callum Slaney-Flett <callum@mobi-market.co.uk>
2020-01-15 11:07:27 +0000

    Add LabelType enum.
    Signed-off-by: Callum Slaney-Flett <callum@mobi-market.co.uk>

---
+++ src/Enum/LabelType.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MobiMarket\RoyalMailShipping\Enum;
+
+class LabelType
+{
+    public const PDF        = 'PDF';
+    public const PNG        = 'PNG';
+    public const DATASTREAM = 'DATASTREAM';
+    public const ZPL203DPI  = 'ZPL203DPI';
+    public const ZPL300DPI  = 'ZPL300DPI';
+}
